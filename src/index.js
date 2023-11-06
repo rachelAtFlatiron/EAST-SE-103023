9978b7900fc75690e2bedf287c0730db375a0c28
Thomas Wolf <thomasrussellwolf@gmail.com>
2023-11-06 09:43:26 -0600

    pre-lecture init

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ const bookList = document.querySelector("#book-list");
 const toggleBookFormButton = document.querySelector("#toggle-form");
 const bookForm = document.querySelector("#book-form"); 
 const toggleStoreFormButton = document.querySelector("#toggle-store-form");
+const url = "http://localhost:3000";
+
 //✅ 3. store drop-down list
 //✅ 3. store-form const
 
 //✅ 2. include POST request on new book form submit
   //✅ 2a. create fetch request: pessimistic and optimistic approach
   //✅ 2b. use boilerplate from request_helpers.js to execute POST request
-	//✅ 2c. use dev tools to simulate a slow collection (network) and add a loading class to li 
-  
 
 //✅ 3. implement store form and store dropdown
 //✅ 3a. create eventListener for form
@@ -23,6 +23,7 @@ const toggleStoreFormButton = document.querySelector("#toggle-store-form");
 fetch(`${url}/books`)
 	.then((res) => res.json())
 	.then((books) => books.forEach((book) => renderBook(book)));
+
 getJSON(`${url}/stores`)
 .then(stores => {
 	renderStoreSelectionOptions(stores)
