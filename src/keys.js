74aedfef596f725d3d201bc716a6202f4a7e8671
rachelAtFlatiron <102536075+rachelAtFlatiron@users.noreply.github.com>
2023-11-09 16:05:05 -0500

    Delete src/keys.js

--- src/keys.js
+++
@@ -1,9 +0,0 @@
-//✅ 2. create keys.js file to hold API key for Google books
-
-
-//✅ 2a. save API_KEY
-
-//✅ 2b. create .gitignore to ignore keys.js
-
-//✅ 3. use google books api to get information in browser
-//✅ 3a. test getting information from the API in browser
